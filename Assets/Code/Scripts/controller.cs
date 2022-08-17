6b660eae65fbf59f84f9ef40298b6f1825ceed55
Nolan <nolim7@yahoo.com>
2022-08-17 17:07:50 +0200

    Character Animation

--- Assets/Code/Scripts/controller.cs
+++ Assets/Code/Scripts/controller.cs
@@ -13,8 +13,11 @@ public class controller : MonoBehaviour
     GameObject bullet;
     public GameObject projectile_stocke;
     // Start is called before the first frame update
+    Animator anim;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -25,6 +28,7 @@ public class controller : MonoBehaviour
         vertical = Input.GetAxis("Vertical") * MultipleSpeed;
 
         if (Input.GetButtonDown("Fire1")) {
+            anim.SetTrigger("Catch");
             GameObject NewBullet = Instantiate(bullet, transform.position, Quaternion.identity);
             NewBullet.GetComponent<Rigidbody2D>().velocity = new Vector3 (2, 0, 0);
             NewBullet.GetComponent<Bullet>().controller_ = this;
@@ -33,7 +37,13 @@ public class controller : MonoBehaviour
 
     void FixedUpdate()
     {
+        
+
         rigidbody2D.velocity = new Vector2(horizon, vertical);
+        anim.SetFloat("velocity_x", rigidbody2D.velocity.x);
+          anim.SetFloat("velocity_y", rigidbody2D.velocity.y);
+        if(((int)horizon) != 0)
+            anim.SetInteger("direction", (int)horizon);
     }
 
 }
