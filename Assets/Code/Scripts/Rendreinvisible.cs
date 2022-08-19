638030f8bda235e187c84e88f2bee76faf70c0d1
Eloi <eloibruegger@gmail.com>
2022-08-19 23:25:22 +0200

    level 4

--- Assets/Code/Scripts/Rendreinvisible.cs
+++ Assets/Code/Scripts/Rendreinvisible.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class Rendreinvisible : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriterenderer;
+    [SerializeField] SpriteRenderer Visible;
+    [SerializeField] SpriteRenderer Vivivivisible;
+    private void Start()
+    {
+        if (Visible != null)
+        {
+            Visible.enabled = false;
+        }
+        if (Vivivivisible != null)
+        {
+            Vivivivisible.enabled = false;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag ("avalable"))
         {
             spriterenderer.enabled = false;
+            if (Visible != null)
+            {
+                Visible.enabled = true;
+            }
+            spriterenderer.enabled = false;
+            if (Vivivivisible != null)
+            {
+                Vivivivisible.enabled = true;
+            }
         }
     }
 }
