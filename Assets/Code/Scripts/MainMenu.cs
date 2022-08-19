1055e2952392a80e3d4ad79a1830b234de6d5ba3
Lara <Laraakladios@gmail.com>
2022-08-19 17:30:49 +0200

    écran victoire + boutons back + son menu

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -47,6 +47,9 @@ public class MainMenu : MonoBehaviour
             {
                 button.interactable = true;
             }
+            
+   
+    
         }
        
     }
---
+++ Assets/Code/Scripts/MenuLara.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLara : MonoBehaviour
+{
+    public void Playgame ()
+{
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+
+public void QuitGame ()
+{
+Debug.Log("Quit!");
+Application.Quit();
+}
+
+    public void ShowMenu ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMenu ()
+    {
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("menu");
+
+    }
+
+
+
+}
